323f858deb6c358e78da4969d3901c5f3a84005c
Youssef <youssefyaser2020@gmail.com>
2025-05-03 08:03:26 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Cards from "./Components/Cards";
+import React from "react";
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Cards />
+      </div>
+    );
+  }
+}
+
+export default App;
